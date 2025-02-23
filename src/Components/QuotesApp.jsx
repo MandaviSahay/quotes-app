fcadb0c415174eb77bd800d883286377e9eda05c
mandavi01 <mandavi.sahay@learnbay.co>
2025-02-23 22:41:53 +0530

    second commit

--- src/Components/QuotesApp.jsx
+++ src/Components/QuotesApp.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import './QuotesApp.css'
 const QuotesApp = () => {
+  const [quote, setQuote] =useState({
+    text:"Ask not what your country can do for you; ask what you can do for your country",
+    author:"John Keneddy"
+  })
+  const [favourites, setFavourites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false)
+
+  const fetchNewQuote = async () => {
+    const url = 'https://dummyjson.com/quotes/random'
+    const response = await fetch(url)
+    const data = await response.json()
+    setQuote({
+      text: data.quote,
+      author: data.author,
+    })
+  }
+  const toggleFavorites = () => {
+    setShowFavorites(!showFavorites)
+  }
+  const addToFavourites = ()=>{
+    setFavourites([...favourites, quote])
+  }
   return (
     <div className='container'>
       <div className="quotes-app">
         <h1 className="app-heading">Quote.</h1>
-        <i className='bx bxs-heart fav-icon'></i>
+        <i className='bx bxs-heart fav-icon'onClick={toggleFavorites}></i>
         <div className='quote'>
             <i className='bx bxs-quote-alt-left left-quote'></i>
-            <p className="quote-text">Ask not what your country can do for you; ask what you can do for your country</p>
-            <p className="quote-author">John Keneddy</p>
+            <p className="quote-text">{quote.text}</p>
+            <p className="quote-author">{quote.author}</p>
             <i className='bx bxs-quote-alt-right right-quote'></i>
         </div>
         <div className="circles">
@@ -18,9 +41,33 @@ const QuotesApp = () => {
             <div className="circle-4"></div>
         </div>
         <div className="buttons">
-            <button className="btn btn-new">New Quote</button>
-            <button className="btn btn-fav">Add to Favourites</button>
+            <button className="btn btn-new" onClick={fetchNewQuote}>New Quote</button>
+            <button className="btn btn-fav" onClick={addToFavourites}>Add to Favourites</button>
         </div>
+        {showFavorites &&(
+          <div className="favourites">
+          <button className="btn-close" onClick={toggleFavorites}>
+            <i className='bx bx-x'></i>
+          </button>
+          {
+            favourites.map((favQuote, index)=>(
+              <div className="fav-quote" key={index}>
+            <div className="fav-quote-delete" >
+              <i className='bx bx-x-circle'></i>
+            </div>
+            <div className="fav-quote-content">
+              <div className="fav-quote-text">
+              {favQuote.text}
+              </div>
+              <div className="fav-quote-author">{favQuote.author}</div>
+            </div>
+          </div>
+            ))
+          }
+          
+        </div>
+        )}
+        
       </div>
     </div>
   )
